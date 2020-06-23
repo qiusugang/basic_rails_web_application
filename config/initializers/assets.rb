d53a1bdcad0be59346d4e656b5ed3b7c150b8c39
qiusugang <qiusugang@qq.com>
2020-06-23 21:37:19 +0800

    add vue on rails

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
+Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( ckeditor/* )
--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -27,7 +27,7 @@ Ckeditor.setup do |config|
 
   # Override parent controller CKEditor inherits from
   # By default: 'ApplicationController'
-  config.parent_controller = 'CkController'
+  # config.parent_controller = 'MyController'
 
   # Asset model classes
   # config.picture_model { Ckeditor::Picture }
@@ -49,7 +49,7 @@ Ckeditor.setup do |config|
   # CKEditor CDN
   # More info here http://cdn.ckeditor.com/
   # By default: nil (CDN disabled)
-  # config.cdn_url = '//cdn.ckeditor.com/4.6.1/standard/ckeditor.js'
+  # config.cdn_url = '//cdn.ckeditor.com/4.7.1/standard/ckeditor.js'
 
   # JS config url
   # Used when CKEditor CDN enabled
