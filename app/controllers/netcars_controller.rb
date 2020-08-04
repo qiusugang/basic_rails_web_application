062e3621abd17c5b40d6d5e38c9fd45c4f6ed901
qiusugang <qiusugang@qq.com>
2020-08-04 15:56:27 +0800

    Change total result

--- app/controllers/netcars_controller.rb
+++ app/controllers/netcars_controller.rb
@@ -5,8 +5,9 @@ class NetcarsController < ApplicationController
   def index
     query_str = params[:search]
     if params[:search] && params[:search].strip != ''
-      result = NetCar.search(query_str,0,20)
-      json_text = '{"object":"list","total_cards":20,"has_more":false,"data":' + result.records.to_json(:only => [:id, :car_name, :city_name, :purchase_date, :mileage, :detail_link, :price]) + '}'
+      result = NetCar.search(query_str,0,100)
+      size = result.records.size
+      json_text = '{"object":"list","total_cards":' + size.to_s + ',"has_more":false,"data":' + result.records.to_json(:only => [:id, :car_name, :city_name, :purchase_date, :mileage, :detail_link, :price]) + '}'
       render :plain => json_text
     else
       render :plain => "Not found", :status => 404 
